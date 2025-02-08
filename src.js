e01d15b3b3a0ba22cf6c741f12f9ff26d2fe2201
Daniel <d.popov0470@gmail.com>
2025-02-08 02:31:04 +0200

    feat: add update display functionality

--- src.js
+++ src.js
@@ -1,17 +1,21 @@
+const keypad = document.querySelector(".calculator-keypad");
+const display = document.querySelector(".calculator-display");
+keypad.addEventListener("click", updateDisplay);
+
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
-function subtract(a,b) {
-    return a - b;
+function subtract(a, b) {
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
-function divide (a, b) {
-    return a / b;
+function divide(a, b) {
+  return a / b;
 }
 
 let operandOne;
@@ -29,4 +33,14 @@ function operate(a, b, operator) {
         case '/': return divide (a, b);
         break;
     }
+}
+function updateDisplay(e) {
+  let digit = e.target.innerText;
+  if (e.target.classList.contains("digit")) {
+    if (display.innerText === "0") {
+      display.innerText = "";
+    }
+
+    display.innerText += digit;
+  }
 }
